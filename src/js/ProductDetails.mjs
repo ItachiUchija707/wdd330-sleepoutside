7b3348ccc1d12caf1d8f8ab87c8e67a104c8587e
T-017 <jholt2@byupathway.edu>
2026-09-10 21:59:46 -0500

    prod details

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,10 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.dataSource = dataSource;
+    this.product = {};
+  }
+}
