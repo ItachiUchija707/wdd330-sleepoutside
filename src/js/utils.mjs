05fa4c9aefe18ceca2625525c6b62e4545c0d5bd
guifabri <erickscala7025@gmail.com>
2026-09-12 00:51:42 -0400

    es-individual2-finished

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -27,3 +27,17 @@ export function getParam(param) {
   const product = urlParams.get('product')
   return product;
 }
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = true,
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  const htmlStrings = list.map(templateFn);
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
